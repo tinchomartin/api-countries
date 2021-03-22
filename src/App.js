6999c8e560b99057b96b2053892befc03b08ed94
Martin <tincho.htmlqgmail.com>
2021-03-22 11:57:02 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Countries from "./components/Countries";
+
+function App() {
+  return (
+    <div className="App">
+     <Countries />
+    </div>
+  );
+}
+
+export default App;
