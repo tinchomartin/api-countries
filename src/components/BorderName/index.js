bc792627c43f384af07978f42c0ad69478a4d34e
Martin <tincho.html@gmail.com>
2021-09-15 20:32:41 -0300

    modificacion card country

--- src/components/BorderName/index.js
+++ src/components/BorderName/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 
 import axios from "axios";
 
@@ -17,8 +18,15 @@ export default function BorderNames(props) {
       .then((res) => setNameBorder(res.data));
   }, [border]);
   return (
-    <Link to={`/search/${country.alpha3Code}`} className="shadow">
-      {country.name}
+    <Link to={`/search/${country.alpha3Code}`}>
+      <Box
+        as="span"
+        boxShadow="rgba(0, 0, 0, 0.1) 0px 0px 8px 0px"
+        padding="0.2rem 1.5rem"
+        text-decoration="none"
+      >
+        {country.name}
+      </Box>
     </Link>
   );
 }
